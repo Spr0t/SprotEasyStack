0c25814f25fbb34ca4c56675eff4b5e2da2ca908
Serj Prots <serganas995@gmail.com>
2022-06-02 17:19:15 +0300

    Добавьте файлы проекта.

---
+++ SprotEasyStack/Program.cs
@@ -0,0 +1,26 @@
+﻿using SprotEasyStack.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SprotEasyStack
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            EasyStack<int> easyStack = new EasyStack<int>();
+
+            easyStack.Push(1);
+            easyStack.Push(2);
+            easyStack.Push(3);
+            var item =easyStack.Pop();
+            var item2 = easyStack.Peek();
+            Console.WriteLine(item);
+            Console.WriteLine(item2);
+            Console.ReadLine();
+        }
+    }
+}
